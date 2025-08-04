13325a7ca0b817eb1c3a8bd7d6f0c1976aec9a00
oliver <gokohl@gmail.com>
2025-08-04 12:59:07 +0200

    migration to 11ty

---
+++ README.md
@@ -0,0 +1,8 @@
+# Website for oliverkohl.com
+
+build using 11ty & Bulma
+
+## Commands
+- ```npm install``` to install dependencies
+- ```npm run build``` for building artefacts 
+- ```npm run serve``` to run vite preview server
---
+++ src/_data/site.js
@@ -0,0 +1,12 @@
+export default {
+  title: 'Oliver Kohl',
+  description: 'Experience Engineer',
+  url: 'https://oliverkohl.com/',
+  keywords: "",
+  image: "",
+  seoimage: "",
+  // baseUrl: '/',
+  // author: '',
+  // authorTwitter: '',
+  buildTime: new Date()
+};
